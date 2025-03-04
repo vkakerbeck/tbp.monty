d53d8ed72c946fff9b669833d13738418968bb7f
Tristan Slominski <tslominski@thousandbrains.org>
2025-03-04 11:18:10 -0600

    build: tag version commit with version tag (#203)

--- .github/actions/version_updated/check_version_updated.sh
+++ .github/actions/version_updated/check_version_updated.sh
@@ -5,6 +5,11 @@ do
   echo $changed_file
   if [[ $changed_file == src/tbp/monty/__init__.py ]]; then
     echo "version_updated=true" >> $GITHUB_OUTPUT
+    # grep finds the version line. It will only have two quotes. Then awk uses
+    # the quotes as separators and prints the middle value, which corresponds to
+    # the version inside the quotes.
+    version=$(grep '__version__\s*=\s*' src/tbp/monty/__init__.py | awk -F'"' '{print $2}')
+    echo "version=$version" >> $GITHUB_OUTPUT
     exit 0
   fi
 done < changed_files.txt
