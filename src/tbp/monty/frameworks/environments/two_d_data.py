09b88441ae0dbb0ed0455f1438c2c86f688a72be
vclay <vkakerbeck@uos.de>
2025-04-11 15:23:19 +0300

    simplify amount aextraction

--- src/tbp/monty/frameworks/environments/two_d_data.py
+++ src/tbp/monty/frameworks/environments/two_d_data.py
@@ -118,7 +118,7 @@ class OmniglotEnvironment(EmbodiedEnvironment):
         Returns:
             observation (dict).
         """
-        amount = 1 if action.rotation_degrees < 1 else action.rotation_degrees
+        amount = max(action.rotation_degrees, 1)
         self.step_num += int(amount)
         query_loc = self.locations[self.step_num % self.max_steps]
         patch = self.get_image_patch(
