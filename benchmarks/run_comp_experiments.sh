38b91850848d155736dec4a714d36b60573b9732
vclay <vkakerbeck@uos.de>
2025-09-12 10:26:03 +0100

    update bash script

--- benchmarks/run_comp_experiments.sh
+++ benchmarks/run_comp_experiments.sh
@@ -10,4 +10,7 @@ python run.py -e supervised_pre_training_flat_objects_wo_logos \
     supervised_pre_training_objects_with_logos_lvl4_comp_models \
     infer_comp_lvl1_with_monolithic_models \
     infer_parts_with_part_models \
-    infer_comp_lvl1_with_comp_models
+    infer_comp_lvl1_with_comp_models \
+    infer_comp_lvl2_with_comp_models \
+    infer_comp_lvl3_with_comp_models \
+    infer_comp_lvl4_with_comp_models
