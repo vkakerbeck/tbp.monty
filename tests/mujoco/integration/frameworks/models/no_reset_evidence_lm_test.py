bf239c448789c4b548eeeb9094a529e809bb5413
Jeremy Shoemaker <jshoemaker@thousandbrains.org>
2026-06-19 12:10:26 -0500

    test: create MuJoCo version of no_reset_evidence_lm_test.py (#1040)

---
+++ tests/mujoco/integration/frameworks/models/no_reset_evidence_lm_test.py
@@ -0,0 +1,125 @@
+# Copyright 2025-2026 Thousand Brains Project
+#
+# Copyright may exist in Contributors' modifications
+# and/or contributions to the work.
+#
+# Use of this source code is governed by the MIT
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+from __future__ import annotations
+
+import copy
+import shutil
+import tempfile
+
+import hydra
+import numpy as np
+import numpy.typing as npt
+
+from tbp.monty.frameworks.experiments.mode import ExperimentMode
+from tests import HYDRA_ROOT
+from tests.unit.resources.unit_test_utils import BaseGraphTest
+
+
+class NoResetEvidenceLMTest(BaseGraphTest):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.output_dir = tempfile.mkdtemp()
+
+        with hydra.initialize_config_dir(version_base=None, config_dir=str(HYDRA_ROOT)):
+            self.pretraining_cfg = hydra.compose(
+                config_name="experiment",
+                overrides=[
+                    "experiment=test/no_reset_evidence_lm/pretraining_mujoco",
+                    f"experiment.config.logging.output_dir={self.output_dir}",
+                ],
+            )
+            self.unsupervised_cfg = hydra.compose(
+                config_name="experiment",
+                overrides=[
+                    "experiment=test/no_reset_evidence_lm/unsupervised_mujoco",
+                    f"experiment.config.logging.output_dir={self.output_dir}",
+                ],
+            )
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.output_dir)
+
+    def assert_dicts_equal(
+        self,
+        d1: dict[str, npt.NDArray[np.float64]],
+        d2: dict[str, npt.NDArray[np.float64]],
+        msg: str,
+    ) -> None:
+        """Asserts that two dictionaries containing NumPy arrays are equal.
+
+        This method checks that the dictionaries have the same keys and that
+        the corresponding NumPy arrays are element-wise equal.
+
+        Args:
+            d1: The first dictionary to compare.
+            d2: The second dictionary to compare.
+            msg: The message to display if the assertion fails.
+        """
+        self.assertEqual(d1.keys(), d2.keys(), msg)
+        for key, d1_val in d1.items():
+            self.assertTrue(np.array_equal(d1_val, d2[key]), msg)
+
+    def test_no_reset_evidence_lm(self) -> None:
+        """Checks that unsupervised LM does not reset the evidence between episodes.
+
+        Note: We use the default `MontyForEvidenceGraphMatching` and `EvidenceGraphLM`
+        to train a Monty Experiment, then transfer the pretrained graphs to an
+        unsupervised Inference Experiment. Disabling the reset logic does not support
+        training at the moment.
+        """
+        train_exp = hydra.utils.instantiate(self.pretraining_cfg.experiment)
+        with train_exp:
+            train_exp.run()
+
+        eval_exp = hydra.utils.instantiate(self.unsupervised_cfg.experiment)
+        with eval_exp:
+            # load the eval experiment with the pretrained models
+            pretrained_models = train_exp.model.learning_modules[0].state_dict()
+            eval_exp.model.learning_modules[0].load_state_dict(pretrained_models)
+
+            eval_exp.experiment_mode = ExperimentMode.EVAL
+            eval_exp.model.set_experiment_mode(eval_exp.experiment_mode)
+            eval_exp.pre_epoch()
+
+            lm = eval_exp.model.learning_modules[0]
+
+            # first episode
+            self.assertEqual(
+                len(lm._hypotheses),
+                0,
+                "evidence dict should be empty before the first episode",
+            )
+            eval_exp.pre_episode()
+            episode_1_steps = eval_exp.run_episode_steps()
+            eval_exp.post_episode(episode_1_steps)
+            post_episode1_evidence = copy.deepcopy(
+                {graph_id: hyp.evidence for graph_id, hyp in lm._hypotheses.items()}
+            )
+            self.assertGreater(
+                len(post_episode1_evidence),
+                0,
+                "evidence dict should now contain evidence values of the first episode",
+            )
+
+            # second episode
+            eval_exp.pre_episode()
+            self.assert_dicts_equal(
+                post_episode1_evidence,
+                {graph_id: hyp.evidence for graph_id, hyp in lm._hypotheses.items()},
+                "evidence dict should not change between episodes",
+            )
+            episode_2_steps = eval_exp.run_episode_steps()
+            eval_exp.post_episode(episode_2_steps)
+            self.assertGreater(
+                len(lm._hypotheses),
+                0,
+                "evidence dict should contain evidence values",
+            )
+            eval_exp.post_epoch()
